eb6510fe04185fb26d5ca8320d7360afce761ea7
sonali.rai <sonalirai29jan@gmail.com>
2026-06-11 15:04:00 +0530

    java application

---
+++ docker-java-app/Hello_World.java
@@ -0,0 +1,15 @@
+public class Hello_World {
+public static void main (String args[]){
+
+System.out.println("Helloooooooo");
+System.out.println("Welcome to Java");
+System.out.println();
+for(int i=0;i<5;i++){
+
+System.out.println("Message" + i);
+
+
+}
+}
+
+}
